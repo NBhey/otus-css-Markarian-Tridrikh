63d6d4c0b96bc075f52d25067dce093efc7fabdc
Oganes <og.marckaryan@yandex.ru>
2023-04-20 13:08:21 +0300

    automatization v1.0

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./style/style.css";
+
+
+//import lodash from ('lodash')
+
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    target: 'web',
+    devtool: 'source-map',
+    entry: { 
+       main: path.resolve(__dirname,'./src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].[hash-8].js',
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'), 
+             filename: 'index.html', 
+    }),
+    ],
+    module: {
+        rules: [
+        {
+            test: /\.(sass|css)$/i,
+            use: ["style-loader", "css-loader", "node-sass" ],
+
+        },
+        {
+             test: /\.html$/,
+            use: "html-loader",
+        },
+        {
+            test:/\.(?:ico|gif|png|jpeg|jpg)$/i,
+            type: 'asset/resource',
+            generator:{
+                filename:'images/[name]-[hash:5][ext]',
+            }
+        },
+        {
+            test:/\.(woff(2)?|eot|ttf|otf|svg)$/,
+            type: 'asset/resource',
+            generator:{
+                filename:'fonts/[name]-[hash:5][ext]',
+            }
+        },
+        ]
+    },
+    devServer: {
+        compress: false, 
+        open: true, 
+        port:3000, 
+        hot:true,
+    }  
+  }
